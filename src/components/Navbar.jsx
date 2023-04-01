166e2d25f1815b965d4bddc8883dcfd877d9cfde
Harsh Rane <hharshup@gmail.com>
2023-04-02 00:26:00 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div >
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+        <div> 
+            <button>Sign In</button>
+            <button>Sign Up</button>
+        </div>
+        
+        
+    </div>
+    
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsxml}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
